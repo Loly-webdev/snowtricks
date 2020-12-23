73733190e70e7774eba2a264c24ebbdf855722b9
LolyWd <eloise.ruiz.rodriguez@gmail.com>
2020-12-23 12:28:20 +0100

    creation controllers and actions to the page of the blog

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,22 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './styles/global.scss';
 
-// start the Stimulus application
-import './bootstrap';
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
+
+// navItemActive
+//let page = location.pathname.split('/').slice(-1);
+//$('nav ul li').removeClass('active bg-gradient');
+//$('#' + page).addClass('active bg-gradient');
